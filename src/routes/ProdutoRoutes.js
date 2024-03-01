0d2b6d899c88142983c7da4ab952b9a24648d3f0
YuriAPCarvalho <yuri.apcarvalho@gmail.com>
2024-02-29 21:46:28 -0400

    ajuste deletar foto produto

--- src/routes/ProdutoRoutes.js
+++ src/routes/ProdutoRoutes.js
@@ -81,18 +81,24 @@ router.put("/:id", upload.single("fotoProduto"), async (req, res) => {
   console.log(req.file);
   const { id } = req.params;
   const produto = req.body;
+
   if (req.file) {
     produto.fotoProduto = req.file.buffer;
   }
+
+  if (req.body.deletePhoto === 'true') {
+    produto.fotoProduto = null;
+  }
+
   try {
     const result = await ProdutoService.updateProduto(id, produto);
     if (!result) {
-      res.status(404).json({ message: `produto com id ${id} não encontrado.` });
+      res.status(404).json({ message: `Produto com id ${id} não encontrado.` });
     } else {
       res.json(result);
     }
   } catch (error) {
-    res.status(500).json({ message: "Erro ao atualizar produto.", error });
+    res.status(500).json({ message: "Erro ao atualizar produto.", error: error.message });
   }
 });
 
